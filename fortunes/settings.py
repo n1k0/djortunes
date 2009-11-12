b9c59b6a9e658940b3b40f125b4b13a08d5cb3ab
Nicolas Perriault <nperriault+github@gmail.com>
2009-11-12 12:46:01 +0100

    migrated templates to use html5, moved stylesheet to a dedicated static file, added an app specific settings configuration file

---
+++ fortunes/settings.py
@@ -0,0 +1,3 @@
+import os
+
+STATIC_DOC_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media')
--- fortunes/urls.py
+++ fortunes/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
+from djortunes.fortunes import settings as fsettings
 
 urlpatterns = patterns('djortunes.fortunes.views',
     url(r'^(?P<ftype>(top|worst)?)$', 'index', name = "fortunes-index"),
@@ -6,3 +8,8 @@ urlpatterns = patterns('djortunes.fortunes.views',
     url(r'^show/(?P<fortune_id>\d+)$', 'detail', name = "fortune-detail"),
     url(r'^vote/(?P<fortune_id>\d+)/(?P<direction>(up|down))$', 'vote', name = "fortune-vote"),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.static',
+        (r'^static/(?P<path>.*)$', 'serve', {'document_root': fsettings.STATIC_DOC_ROOT}),
+    )
