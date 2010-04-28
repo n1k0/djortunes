8d3c5d1ad4ea6c2c17a714feda4091435d9cdbd8
Nicolas Perriault <nperriault+github@gmail.com>
2010-04-28 23:56:56 +0200

    implemented a better unique slugs management system, added tests

--- fortunes/models.py
+++ fortunes/models.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
@@ -11,13 +11,28 @@ class Fortune(models.Model):
     title = models.CharField(max_length=200, blank=False)
     slug = models.SlugField(_('slug'), db_index=True, max_length=255, unique_for_date='pub_date')
     content = models.TextField(blank=False)
-    pub_date = models.DateTimeField(_('published date'), default=datetime.datetime.now())
+    pub_date = models.DateTimeField(_('published date'), default=datetime.now())
     votes = models.IntegerField(default=0)
 
     objects = FortuneManager()
 
     def __unicode__(self):
         return _("%s, from %s") % (self.title, self.author)
+
+    def check_slug(self):
+        "If no slug has been generated yet, tries to generate a unique one"
+        if not self.slug:
+            prefix = ""
+            i = 0
+            while True:
+                self.slug = prefix + slugify(self.title)
+                try:
+                    Fortune.objects.values('id').get(slug=self.slug)
+                    i += 1
+                    prefix = str(i) + "-"
+                except Fortune.DoesNotExist:
+                    break
+        return self.slug
 
     @models.permalink
     def get_absolute_url(self):
@@ -29,7 +44,5 @@ class Fortune(models.Model):
         })
 
     def save(self):
+        self.check_slug()
         super(Fortune, self).save()
-        if self.id and not self.slug:
-            self.slug = str(self.id) + '-' + slugify(self.title)
-            super(Fortune, self).save()
--- fortunes/tests.py
+++ fortunes/tests.py
@@ -1,11 +1,21 @@
-from django.test import TestCase
-from djortunes.fortunes.templatetags.fortune_extras import fortunize
+import unittest
 
-__test__ = {"doctest": """
-fortunize() tests:
+class FortuneExtraTest(unittest.TestCase):
+    def test_fortunize(self):
+        from djortunes.fortunes.templatetags.fortune_extras import fortunize
+        self.assertEquals(fortunize(u'<niko> foo'), u'<dl><dt class="odd">&lt;niko&gt;</dt><dd><q>foo</q></dd>\n</dl>')
+        self.assertEquals(fortunize(u'<niko> foo\n<david> bar'), u'<dl><dt class="odd">&lt;niko&gt;</dt><dd><q>foo</q></dd>\n<dt class="even">&lt;david&gt;</dt><dd><q>bar</q></dd>\n</dl>')
 
->>> fortunize('<niko> foo')
-u'<dl><dt class="odd">niko</dt><dd><q>foo</q></dd>\n</dl>'
->>> fortunize(u'<niko> foo\\n<david> bar')
-u'<dl><dt class="odd">niko</dt><dd><q>foo</q></dd>\n<dt class="even">david</dt><dd><q>bar</q></dd>\n</dl>'
-"""}
+class FortuneModelTest(unittest.TestCase):
+    def test_check_slug(self):
+        import datetime
+        from djortunes.fortunes.models import Fortune
+        f1 = Fortune(title='plop', pub_date=datetime.datetime.now())
+        f1.save()
+        self.assertEquals(f1.slug, 'plop')
+        f2 = Fortune(title='plop', pub_date=datetime.datetime.now())
+        f2.save()
+        self.assertEquals(f2.slug, '1-plop')
+        f3 = Fortune(title='plop', pub_date=datetime.datetime.now())
+        f3.save()
+        self.assertEquals(f3.slug, '2-plop')
