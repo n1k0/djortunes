0e25806ef24d47b56783acb6ead86d7168bcd141
Nicolas Perriault <nperriault+github@gmail.com>
2010-05-01 15:32:11 +0200

    added feeds module

---
+++ django_fortunes/feeds.py
@@ -0,0 +1,40 @@
+from django.contrib.syndication.views import Feed
+from django.shortcuts import get_object_or_404
+from django_fortunes.models import Fortune
+from django_fortunes.templatetags.fortune_extras import fortunize
+
+class FortuneFeed(Feed):
+    author_name = 'The django_fortunes application'
+    link = 'http://github.com/n1k0/djortunes'
+    manager = Fortune.objects
+
+    def item_author_name(self, item):
+        return item.author
+
+    def item_description(self, item):
+        return fortunize(item.content)
+
+    def item_link(self, item):
+        return item.get_absolute_url()
+
+    def item_pubdate(self, item):
+        return item.pub_date
+
+    def item_title(self, item):
+        return item.title
+
+class LatestFortunes(FortuneFeed):
+    title = "Latest fortunes"
+    link = "/fortunes/"
+    description = "Latest fortunes added."
+
+    def items(self):
+        return self.manager.latest(5)
+
+class LatestFortunesByAuthor(FortuneFeed):
+    title = "Latest fortunes by XXX"
+    link = "/fortunes/"
+    description = "Latest fortunes added by XXX."
+
+    def items(self):
+        return self.manager.latest_by_author('XXX', 5)
